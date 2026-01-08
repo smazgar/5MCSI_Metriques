f7dfcad5806f9fd316bb8f1c251b7208a28c5027
smazgar <90338871+smazgar@users.noreply.github.com>
2026-01-08 12:13:10 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
 
-@app.route("/contact/") #a jour
+@app.route("/contact/")
 def MaPremiereAPI():
     return render_template("contact.html")
   
