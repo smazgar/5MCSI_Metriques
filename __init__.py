0e2e4eccfb5131e5530d1eb924832c88d61907c5
smazgar <90338871+smazgar@users.noreply.github.com>
2026-01-08 11:37:52 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,10 @@ def mongraphique():
 def monhistogramme():
     return render_template("histogramme.html")
   
+@app.route("/contact/")
+def MaPremiereAPI():
+    return render_template("contact.html")
+  
 if __name__ == "__main__":
   app.run(debug=True)
 
