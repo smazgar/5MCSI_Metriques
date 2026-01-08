66b6d3c7e0cc85b7e8292a791ae067c9aa0ac444
smazgar <90338871+smazgar@users.noreply.github.com>
2026-01-08 11:17:32 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,7 @@ def MaPremiereAPI():
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #Comm2
+  
   @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
